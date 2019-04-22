3f2c218c000024e399d8f803804ea7acd3746ef7
Josh Williams <joshua.frank.williams@gmail.com>
2019-04-22 21:22:51 +0000

    doxygen testing

--- sched.h
+++ sched.h
@@ -1,6 +1,14 @@
+/**
+  @file sched.h
+  @brief Definitions for new scheduling policies.
+*/
+
 #ifndef SCHED_H
 #define SCHED_H
 
+/**
+  @brief Enum containing different scheduling policy options.
+*/
 enum schedpolicy_lab3 { SCHED_RR, SCHED_FIFO, SCHED_OTHER };
 
 #endif
